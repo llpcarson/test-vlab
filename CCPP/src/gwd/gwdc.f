2be5c408e78088cae17715cf7576981119d622b1
Man.Zhang <Man.Zhang@noaa.gov>
2016-06-17 21:30:07 -0600

    minor changes in gwdc.f

--- CCPP/src/gwd/gwdc.f
+++ CCPP/src/gwd/gwdc.f
@@ -19,12 +19,7 @@
 !! One way is to specify a vertical structure of the momentum flux normalized by the cloud-top value,
 !! similar to what has been done for mountain drag parameterization. The other way is to apply the wave
 !! saturation hypothesis in order to find wave breaking levels in terms of the Richardon number
-!! criterion using the nonlinearity factor of thermally induced waves. That is, if \f$Ri_{min}\geq1/4\f$,
-!! the momentum flux at a certain level above the cloud top is equal to that below that level, while
-!! if \f$Ri_{min}<1/4\f$, the saturation momentum flux given by (25) is used. This procedure is repeated
-!! until the model top is reached. At the model top, the momentum flux can be specified as
-!! a value just below it. The method employing the wave saturation hypothesis in GFS physics is
-!! essentially the same as that which has been used in mountian wave drag parameterization (Kim 1996 \cite kim_1996).
+!! criterion using the nonlinearity factor of thermally induced waves. 
 !!@{
 
 !> \param[in] IM       horizontal number of used pts
